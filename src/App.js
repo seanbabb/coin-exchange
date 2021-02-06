95b0ec01c788c943974f5276b36fdc7d2132d35e
Sean Babb <sbabb.nj@gmail.com>
2021-02-06 11:42:55 -0500

    API and functional component update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AccountBalance from './components/AccountBalance/AccountBalance';
 import CoinList from './components/CoinList/CoinList';
 import AppHeader from './components/AppHeader/AppHeader';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const AppDiv = styled.div`
   text-align: center;
@@ -10,11 +11,15 @@ const AppDiv = styled.div`
   color: #cccccc;
 `;
 
+const COIN_COUNT = 10;
+const formatPrice = price => parseFloat(Number(price).toFixed(4));
+
 class App extends React.Component {
   state = {
     showBalance: true,
     balance: 10000,
     coinData: [
+      /*
       {
         name: 'Bitcoin',
         ticker: 'BTC',
@@ -45,18 +50,42 @@ class App extends React.Component {
         balance: 0,
         price: 298.99
       },
+      */
     ]
   }
+  
+  componentDidMount = async () => {
+    const response = await axios.get('https://api.coinpaprika.com/v1/coins');
+    const coinIds = response.data.slice(0, COIN_COUNT).map(coin => coin.id);
+    const tickerUrl = 'https://api.coinpaprika.com/v1/tickers/';
+    const promises = coinIds.map(id => axios.get(tickerUrl + id));
+    const coinData = await Promise.all(promises);
+    const coinPriceData = coinData.map(function(response) {
+      const coin = response.data;
+      return {
+        key: coin.id,
+        name: coin.name,
+        ticker: coin.symbol,
+        balance: 0,
+        price: formatPrice(coin.quotes.USD.price),
+      };
+    });
+    // Retrieve the prices
+    this.setState({coinData: coinPriceData});
+  }
+
   handleToggleShowBalance = () => {
     this.setState({showBalance: ! this.state.showBalance});
   }
  
-  handleRefresh = (valueChangeTicker) => {
+  handleRefresh = async (valueChangeId) => {
+    const tickerUrl = `https://api.coinpaprika.com/v1/tickers/${valueChangeId}`;
+    const response = await axios.get(tickerUrl);
+    const newPrice = formatPrice(response.data.quotes.USD.price);
     const newCoinData = this.state.coinData.map( function(values) {
       let newValues = {...values};
-      if (valueChangeTicker === values.ticker){
-        const randomPercentage = 0.995 + Math.random() * 0.01;
-        newValues.price *= randomPercentage;
+      if (valueChangeId === values.key){
+        newValues.price = newPrice;
       }
       return newValues;
     });
--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -8,23 +8,19 @@ const Section = styled.section`
     padding: 1.5rem 0 1.5rem 5rem;
 `;
 
-export default class AccountBalance extends Component {
-   
-    handleClick = (event) => {
+export default function AccountBalance(props) {
+    const handleClick = (event) => {
         event.preventDefault();
-        this.props.handleToggleShowBalance();
+        props.handleToggleShowBalance();
     }
-
-    render() {
-        const buttonText = this.props.showBalance ? 'Hide Balance' : 'Show Balance';
-        const showAccountBalance = this.props.showBalance ? <span>Balance: ${this.props.amount} </span> : null;
-        return (
-            <Section>
-              {showAccountBalance}
-              <button onClick={this.handleClick} >{buttonText}</button>
-            </Section>
-        );
-    }
+    const buttonText = props.showBalance ? 'Hide Balance' : 'Show Balance';
+    const showAccountBalance = props.showBalance ? <span>Balance: ${props.amount} </span> : null;
+    return (
+        <Section>
+            {showAccountBalance}
+            <button onClick={handleClick} >{buttonText}</button>
+        </Section>
+    );
 }
 
 AccountBalance.propTypes = {
